5b684cb534410580cf3511690058b66eecb1e48f
Leo Zhou <glasslion@gmail.com>
2017-03-11 20:36:02 +0800

    add DjangoModelDataSource and matrix transpose support

--- echarts/sources/base.py
+++ echarts/sources/base.py
@@ -3,4 +3,14 @@ from __future__ import absolute_import, unicode_literals
 
 
 class BaseDataSource(object):
-    pass
+
+    @property
+    def T(self):
+        """
+        Perform a matrix transpose on data
+        """
+        self.data = [
+            [row[i] for row in self.data]
+            for i in range(len(self.data[0]))
+        ]
+        return self
--- echarts/sources/django.py
+++ echarts/sources/django.py
@@ -1,1 +1,26 @@
 from __future__ import absolute_import, unicode_literals
+
+from .base import BaseDataSource
+
+
+class DjangoModelDataSource(BaseDataSource):
+    """
+    """
+    def __init__(self, queryset, fields=None):
+        self.queryset = queryset
+        self.fields = fields
+        self.data = self.normalize_data()
+
+    def normalize_data(self):
+        data = [
+            [self.get_field_value(row, field) for field in self.fields]
+            for row in self.queryset
+        ]
+        return data
+
+    def get_field_value(self, row, field):
+        value = getattr(row, field)
+        if callable(value):
+            return value()
+        else:
+            return value
